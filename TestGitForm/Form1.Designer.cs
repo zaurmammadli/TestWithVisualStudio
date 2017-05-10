4239033f046b9acd4c97f1b71a2d093f1110d6d7
Bəylər Həşimli <beyler_heshimov@hotmail.com>
2017-05-10 09:16:58 +0400

    text

--- TestGitForm/Form1.Designer.cs
+++ TestGitForm/Form1.Designer.cs
@@ -33,18 +33,18 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(213, 94);
+            this.button1.Location = new System.Drawing.Point(131, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "text";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(571, 256);
+            this.ClientSize = new System.Drawing.Size(497, 317);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
