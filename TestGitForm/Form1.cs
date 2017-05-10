8a7db11e303059657a745b195a9b660761325d65
zaurmammadli <zaurmemmedli@gmail.com>
2017-05-10 09:15:02 +0400

    Button added

--- TestGitForm/Form1.cs
+++ TestGitForm/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Principal;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,11 @@ namespace TestGitForm
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello " + WindowsIdentity.GetCurrent().Name);
         }
     }
 }
