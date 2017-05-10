6d455efe815646120d031d0a25c0e438eb7d4056
zaurmammadli <zaurmemmedli@gmail.com>
2017-05-10 09:17:30 +0400

    asd

--- TestGitForm/Form1.Designer.cs
+++ TestGitForm/Form1.Designer.cs
@@ -31,15 +31,6 @@
             this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(213, 94);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
